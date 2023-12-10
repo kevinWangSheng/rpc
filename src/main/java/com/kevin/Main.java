beb6be6a8bcf9920cc8e290e2595edcabba29f5d
Kevin <ltwoggbuty2mh@gmail.com>
2023-12-10 17:48:12 +0800

    Initial commit

---
+++ src/main/java/com/kevin/Main.java
@@ -0,0 +1,11 @@
+package com.kevin;
+
+/**
+ * @author wang
+ * @create 2023-2023-10-14:12
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
